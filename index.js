8e49aee5799147088d36b502510aad25d019850a
Zakaria Abdelaziz <35163329+cryptic-dev@users.noreply.github.com>
2021-09-10 19:45:52 +0100

    fixed logic

--- index.js
+++ index.js
@@ -4,54 +4,60 @@ const upper = document.getElementById("up");
 const lower = document.getElementById("low");
 const symbol = document.getElementById("sym");
 const number = document.getElementById("num");
-const gen = document.getElementById("gen");
 
-let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let b = "abcdefghijklmnopqrstuvwxyz";
-let c = "0123456789";
-let d = "!?;:&é/*$<>()-è_çà=";
+let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+let digits = "0123456789";
+let symbols = "!?;:&é/*$<>()-è_çà=";
 
-function GetUpper() {
-    return a[Math.floor(Math.random() * a.length)]
-}
-function GetLower() {
-    return b[Math.floor(Math.random() * b.length)]
-}
-function GetNum() {
-    return c[Math.floor(Math.random() * c.length)]
-}
-function GetSym() {
-    return d[Math.floor(Math.random() * d.length)]
+function getUpper() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
 
-function Generate() {
-    var v = lent.value;
-    let password = "";
-    
-    for (let i = 0; i<v; i++) {
-        const t = getPass()
-        password += t
+function getLower() {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
 
+function getNum() {
+  return digits[Math.floor(Math.random() * digits.length)];
+}
+
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generate() {
+  var v = lent.value;
+
+  if (!v) {
+    pass.innerHTML = "Length is required";
+  }
+
+  //   if (!upper.checked && !lower.checked && !symbol.checked && !number.checked) {
+  //     pass.innerHTML = "You need to select at least one type";
+  //   }
+
+  let password = [];
+
+  for (let i = 0; i < v; i++) {
+    //random number between 0-3; used to choose character type (symbol/lower/digit/lower)
+    let charType = Math.floor(Math.random() * 4);
+
+    console.log(charType);
+    if (upper.checked && charType == 0) {
+      console.log("hna upper");
+      password.push(getUpper());
+    } else if (symbol.checked && charType == 1) {
+      console.log("hna symbol");
+      password.push(getSymbol());
+    } else if (number.checked && charType == 2) {
+      console.log("hna number");
+      password.push(getNum());
+    } else {
+      console.log("hna lower");
+      password.push(getLower());
     }
-    pass.innerHTML = password;
+  }
+
+  pass.innerHTML = password.join("");
 }
-
-function getPass() {
-    let xs = [];
-    if (upper.checked) {
-       return xs.push(GetUpper());
-    }
-    if (lower.checked) {
-        return xs.push(GetLower())
-    }
-    if (symbol.checked) {
-        return xs.push(GetSym())
-    }
-    if (number.checked) {
-        return xs.push(GetNum())
-    }
-    return xs[Math.floor(Math.random() * xs.length)]
-}
-
-gen.addEventListener("click", Generate)
-
